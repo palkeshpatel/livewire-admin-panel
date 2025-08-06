51b90821154f15d25ed7b04108fe0f00ddf0d7df
palkeshpatel <patel.palkesh@gmail.com>
2025-08-06 10:12:33 +0530

    blan-project

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-// Authentication routes
-Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
-// Admin routes (protected)
-Route::prefix('admin')->name('admin.')->middleware('auth')->group(function () {
-    Route::get('/dashboard', function() {
-        return view('layouts.admin');
-    })->name('dashboard');
-    
-    Route::get('/users', function() {
-        return view('layouts.admin');
-    })->name('users');
-    
-    Route::get('/products', function() {
-        return view('layouts.admin');
-    })->name('products');
-    
-    Route::get('/settings', function() {
-        return view('layouts.admin');
-    })->name('settings');
 });
